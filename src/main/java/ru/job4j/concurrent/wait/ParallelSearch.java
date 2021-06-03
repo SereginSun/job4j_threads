49db9d5cc27caf745d3975ee0c3d1cba5f62a401
SereginSun <sereginsun@yandex.ru>
2021-06-03 13:08:52 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/concurrent/wait/ParallelSearch.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent.wait;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(2);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        System.out.println(queue.pool());
+                    }
+                }
+        );
+        consumer.start();
+
+        final Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i != 3 ; i++) {
+                        queue.offer(i);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        );
+        producer.start();
+    }
+}
